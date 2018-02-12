06b1127da5c0fac9f23cd79ef9ea54b945941723
isratnisa <neesha295@gmail.com>
2018-02-12 11:56:21 -0500

    Delete yahoo.sh

--- yahoo.sh
+++
@@ -1,2 +0,0 @@
-#./omp-pmf-train -l 1.2 -k 40 -t 2 ../dataset/Yahoo_music ../dataset/Yahoo_music/test.txt model
-./ccdp_gpu -a 220000 -b 240000 -T 2 -l 1.2 -k 40 -t 2 ../dataset/Yahoo_music ../dataset/Yahoo_music/test.txt model
